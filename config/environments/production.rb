2bdeba7226ada782fa5fb191e6b5276e4cc4a7b9
Bac3Phi <gkztest1@gmail.com>
2018-05-19 00:50:06 +0700

    fix user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 25,
-    domain:               'domain.of.sender.net',
-    user_name:            'dave',
-    password:             'secret',
+    domain:               'sheltered-tundra-91435.herokuapp.com',
+    user_name:            'gikizay@gmail.com',
+    password:             'kkyrxzhiamdfopbr',
     authentication:       'plain',
     enable_starttls_auto: true
   }

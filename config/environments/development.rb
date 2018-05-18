b9a369eef6bc858ed77c8b5e007aa316129c26c1
Bac3Phi <gkztest1@gmail.com>
2018-05-18 23:52:09 +0700

    Fix port to 25

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 25,
     domain:               'sheltered-tundra-91435.herokuapp.com',
     user_name:            'user@website.com',
     password:             'password',

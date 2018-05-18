bcf9177367570ce3714327e8b7ec85294e0f259b
Bac3Phi <gkztest1@gmail.com>
2018-05-19 00:20:58 +0700

    fix dev.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,14 +76,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => "sheltered-tundra-91435.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 25,
- :user_name            => ENV['phipn97@gmail.com'],
- :password             => ENV['pozdien123'],
- :authentication       => "plain",
-:enable_starttls_auto => true
-}
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 25,
+    domain:               'sheltered-tundra-91435.herokuapp.com',
+    user_name:            ENV["phipn97@gmail.com"],
+    password:             ENV["pozdien123"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
